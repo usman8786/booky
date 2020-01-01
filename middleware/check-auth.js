c9bcfb2b1ea320018b0ae418bcd0443f880647ee
Muhammad Usman <usman.mushtaq8786@gmail.com>
2020-01-01 16:19:48 +0500

    first commit

---
+++ middleware/check-auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  
+    try {
+        
+        const token = req.headers.authorization;
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.userData = decoded;
+        next();
+    } catch (error) {
+        
+        next({
+            message: 'Auth failed',
+            status: 401
+        });
+    }
+};
---
+++ routes/books.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
+const bookController = require('../controllers/books.controllers');
+
+
+router.post("/add",checkAuth, bookController.addBook);
+// router.get("/:_id",bookController.getSingleBook);
+router.put("/:_id",checkAuth, bookController.updateBook);
+router.delete("/:_id",checkAuth, bookController.deleteBook);
+router.get("/", checkAuth, bookController.getAll);
+
+ router.get('/:id',checkAuth, bookController.getBooksByUserId);
+ 
+module.exports = router;
