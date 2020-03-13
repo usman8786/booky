53c587c1518e5dafcea4c74c732815af69fad0d8
Muhammad Usman <usman.mushtaq8786@gmail.com>
2020-03-13 15:04:12 +0500

    getting result in adding book revert

--- controllers/books.controllers.js
+++ controllers/books.controllers.js
@@ -43,14 +43,12 @@ booksController.addBook = async (req, res) => {
   try {
     const body = req.body;
     const book = new Books(body);
-    const result = await book.save().then((res)
-    =>{
+    const result = await book.save();
     res.status(200).send({
       result: result,
       code: 200,
       message: "Book Added Successfully"
     });
-  })
   } catch (error) {
     console.log("error", error);
     return res.status(500).send(error);
