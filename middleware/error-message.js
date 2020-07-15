9bc2792a99f2d58988a25960139049cab859f931
Muhammad Usman <usman.mushtaq8786@gmail.com>
2020-07-15 16:02:56 +0500

    undone

--- middleware/error-message.js
+++ middleware/error-message.js
@@ -1,9 +1,6 @@
 module.exports = (error, req, res, next) => {
   if (!error.status) {
     error.status = 500;
-    res.send({
-      message: "middleware",
-    });
   }
   res.status(error.status || 500).send(error);
   next();
